5159c4b21eee17fc774682aa8975e8670751b97a
BhaviniMadhuranath <bhaviniphillippe@gmail.com>
2022-10-22 03:03:47 +0530

    added sha-1

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -1,5 +1,6 @@
 use md5;
 use sha2::{Digest, Sha256};
+use sha1::{Sha1, Digest};
 
 pub fn calc_sha256(filepath: &str) -> String {
     let file_as_bytes = std::fs::read(filepath).unwrap();
@@ -12,5 +13,11 @@ pub fn calc_md5(filepath: &str) -> String {
     let file_as_bytes = std::fs::read(filepath).unwrap();
     let hash = md5::compute(file_as_bytes);
 
+    format!("{:x}", hash)
+}
+pub fn calc_sha1(filepath: &str) -> String {
+    let file_as_bytes = std::fs::read(filepath).unwrap();
+    let hash = Sha1::digest(file_as_bytes);
+    
     format!("{:x}", hash)
 }
