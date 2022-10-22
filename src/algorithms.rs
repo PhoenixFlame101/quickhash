b1c99b1a067983b5d294fa22a8d0c191375b5248
Cubed <47624167+cubed-guy@users.noreply.github.com>
2022-10-22 05:34:04 +0530

    All three algorithms implemented chunk wise
    SHA256, MD5 and SHA1

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -1,16 +1,49 @@
-use md5;
-use sha2::{Digest, Sha256};
+use md5::Md5;
+use sha2::{Sha256, Digest};
+use sha1::Sha1;
+use std::io::{BufRead, BufReader};
+use std::fs::File;
 
 pub fn calc_sha256(filepath: &str) -> String {
-    let file_as_bytes = std::fs::read(filepath).unwrap();
-    let hash = Sha256::digest(file_as_bytes);
+
+    let file = File::open(filepath).unwrap();
+
+    let reader = BufReader::new(file);
+    let mut hasher = Sha256::new();
+    for l_result in reader.lines() {
+        hasher.update(l_result.unwrap().as_bytes());
+        hasher.update(b"\n");
+    }
+    let hash = hasher.finalize();
 
     format!("{:x}", hash)
 }
 
 pub fn calc_md5(filepath: &str) -> String {
-    let file_as_bytes = std::fs::read(filepath).unwrap();
-    let hash = md5::compute(file_as_bytes);
+    let file = File::open(filepath).unwrap();
+
+    let reader = BufReader::new(file);
+    let mut hasher = Md5::new();
+
+    for l_result in reader.lines() {
+        hasher.update(l_result.unwrap().as_bytes());
+    }
+
+    // acquire hash digest in the form of GenericArray,
+    // which in this case is equivalent to [u8; 16]
+    let result = hasher.finalize();
+    format!("{:x}", result)
+}
+
+pub fn calc_sha1(filepath: &str) -> String {
+    let file = File::open(filepath).unwrap();
+
+    let reader = BufReader::new(file);
+    let mut hasher = Sha1::new();
+    for l_result in reader.lines() {
+        hasher.update(l_result.unwrap().as_bytes());
+    }
+    let hash = hasher.finalize();
 
     format!("{:x}", hash)
 }
